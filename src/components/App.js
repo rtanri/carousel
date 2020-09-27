0af7be23748cba9e9c53fc4b16688d3cfb563386
Revin Tanri <tanrirevin@gmail.com>
2020-09-27 19:02:34 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hello World
+      </header>
+    </div>
+  );
+}
+
+
+const carouselData = [
+{
+  title: 'Grab your Beef Burger',
+  description: '',
+},
+{
+  title: 'Choose your spicy level',
+  description: 'From mild to hell fire spicy level',
+},
+{
+  title: 'Unlimited Salad',
+  description: 'No particular limit, all you can eat food on salad bar.',
+},
+{
+  title: 'Happy Hour Specials',
+  description: 'Special 1-for-1 on Beers and Cocktails on Weekday 3pm - 5:30pm',
+}
+];
+
+
+export default App;
